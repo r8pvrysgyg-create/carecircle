f7838e8c5851c5298f6e52b4e18aef61e993bd0d
r8pvrysgyg-create <r8pvrysgyg@privaterelay.appleid.com>
2026-07-02 09:02:59 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 import { setupAuth } from "./auth.js";
 import { addNote, loadNotes } from "./notes.js";
 import { addAppointment, loadAppointments } from "./appointments.js";
+import { addTask, loadTasks } from "./tasks.js";
 
 document.getElementById("addNote").onclick = addNote;
 document.getElementById("addAppt").onclick = addAppointment;
+document.getElementById("addTask").onclick = addTask;
 
 setupAuth(
   async () => {
     await loadNotes();
     await loadAppointments();
+    await loadTasks();
   },
   () => {
     document.getElementById("notes").innerHTML = "";
     document.getElementById("recentNotes").innerHTML = "";
     document.getElementById("appointments").innerHTML = "";
     document.getElementById("todayAppointments").innerHTML = "";
+    document.getElementById("tasks").innerHTML = "";
+    document.getElementById("openTasks").innerHTML = "";
   }
 );
